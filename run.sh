199693ce3fd31e41e47070099c8ceb0334fc426a
Ubuntu <ubuntu@ip-172-31-39-73.eu-west-1.compute.internal>
2020-05-27 13:08:39 +0000

    Fix config

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ export CONTAINER_DISTRO_VERSION=bionic
 
 sudo apt update -y
 sudo apt install -y python3-pip resolvconf
-sudo dpkg-reconfigure --force resolvconf
 
 cd ~/openstack-helm
 
@@ -16,5 +15,4 @@ cd ~/openstack-helm
 dns_cluster_ip=`kubectl get svc kube-dns -n kube-system --no-headers -o custom-columns=":spec.clusterIP"`
 
 echo "nameserver ${dns_cluster_ip}" | sudo tee -a /etc/resolvconf/resolv.conf.d/head > /dev/null
-
-sudo resolvconf -u
+sudo dpkg-reconfigure --force resolvconf
