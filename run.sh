5861b3b166c82a00c7b9764e7903f0bd0e448203
Ubuntu <ubuntu@ip-172-31-34-172.eu-west-1.compute.internal>
2020-05-27 13:43:24 +0000

    Install components

--- run.sh
+++ run.sh
@@ -17,3 +17,14 @@ dns_cluster_ip=`kubectl get svc kube-dns -n kube-system --no-headers -o custom-c
 echo "nameserver ${dns_cluster_ip}" | sudo tee -a /etc/resolvconf/resolv.conf.d/head > /dev/null
 sudo dpkg-reconfigure --force resolvconf
 sudo systemctl restart resolvconf
+
+./tools/deployment/common/setup-client.sh
+./tools/deployment/component/common/ingress.sh
+./tools/deployment/component/common/mariadb.sh
+./tools/deployment/component/common/memcached.sh
+./tools/deployment/component/common/rabbitmq.sh
+./tools/deployment/component/nfs-provisioner/nfs-provisioner.sh
+./tools/deployment/component/keystone/keystone.sh
+./tools/deployment/component/heat/heat.sh
+./tools/deployment/component/glance/glance.sh
+./tools/deployment/component/compute-kit/libvirt.sh
