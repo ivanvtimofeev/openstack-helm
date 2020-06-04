c1d063a4816fc86ba24bb8d3b0393d785360f740
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-06-04 15:24:21 +0300

    Implement the first stage

--- tools/deployment/component/compute-kit/tungsten-fabric.sh
+++ tools/deployment/component/compute-kit/tungsten-fabric.sh
@@ -13,7 +13,8 @@
 #    under the License.
 set -xe
 
-stages="prepare deploy wait"
+stages="prepare deploy"
+OSH_INFRA_PATH=${OSH_INFRA_PATH:="../openstack-helm-infra"}
 
 function show_usage_tf(){
   cat <<EOF
@@ -22,24 +23,61 @@ To use this script pass a stage you need as a first argument:
   tungsten_fabric.sh <stage>
 
 Possible stages are:
+
   'prepare' - prepared host to deploy Tungsten fabric:
-    - add an tf.yaml file to libvirt/values_override
+    - add an tf.yaml file to libvirt/values_overrides
     - comment lines in compute_kit.sh which wait nova and neutron is working and run tests
     - check and add line to /etc/hosts file
+    Run 'preapare' stage after install kubernetes and before run libvirt.sh and compute_kit.sh
+
   'deploy' - deploy Tungsten fabric:
     - download tf Helm charts
     - preapare tf config
     - deploy Tungsten fabric to Kubernetes
-  'wait' - wait for tf and openstack pods are up and works properly:
     - wait for tf pods
     - wait for openstack opds
     - run couple of openstack commands and nova tests
+    Run 'deploy' stage after compute_kit.sh
 EOF
 }
 
 # 'prepare' stage implementation
 function prepare_tf(){
-  echo Prepare tf
+  # add an tf.yaml file to libvirt/values_overrides
+  cat <<EOF > OSH_INFRA_PATH/libvirt/values_overrides/tf.yaml
+network:
+  backend:
+    - tungstenfabric
+dependencies:
+  dynamic:
+    targeted:
+      tungstenfabric:
+        libvirt:
+          daemonset: []
+conf:
+  qemu:
+    cgroup_device_acl: ["/dev/null", "/dev/full", "/dev/zero", "/dev/random", "/dev/urandom", "/dev/ptmx", "/dev/kvm", "/dev/kqemu", "/dev/rtc", "/dev/hpet", "/dev/net/tun"]
+EOF
+
+  # comment lines in compute_kit.sh which wait nova and neutron is working and run tests
+  sed -i 's/^\.\/tools\/deployment\/common\/wait-for-pods.sh openstack/#\.\/tools\/deployment\/common\/wait-for-pods.sh openstack/' ./tools/deployment/component/compute-kit/compute-kit.sh
+  sed -i 's/^openstack compute service list/#openstack compute service list/' ./tools/deployment/component/compute-kit/compute-kit.sh
+  sed -i 's/^openstack hypervisor list/#openstack hypervisor list/' ./tools/deployment/component/compute-kit/compute-kit.sh
+  sed -i 's/^openstack network agent list/#openstack network_agent list/' ./tools/deployment/component/compute-kit/compute-kit.sh
+  sed -i 's/^helm test nova --timeout $timeout/#helm test nova --timeout $timeout/' ./tools/deployment/component/compute-kit/compute-kit.sh
+  sed -i 's/^helm test neutron --timeout $timeout/#helm test neutron --timeout $timeout/' ./tools/deployment/component/compute-kit/compute-kit.sh
+
+  # check and add a line to /etc/hosts file
+  local phys_int=`ip route get 1 | grep -o 'dev.*' | awk '{print($2)}'`
+  local node_ip=`ip addr show dev $phys_int | grep 'inet ' | awk '{print $2}' | head -n 1 | cut -d '/' -f 1`
+
+  if ! cat /etc/hosts | grep "${node_ip}" ; then
+    local tf_hostname=$(hostname)
+    cat <<EOF | sudo tee -a /etc/hosts
+${node_ip} ${tf_hostname}.cluster.local ${tf_hostname}
+EOF
+  fi
+
 }
 
 # 'deploy' stage implementation
@@ -47,10 +85,6 @@ function deploy_tf(){
   echo deploy tf
 }
 
-# 'wait' stage implementation
-function wait_tf(){
-  echo wait tf
-}
 
 if [[ $# == 0 ]] ; then
   echo "ERROR: You have to pass some stage in this script"
